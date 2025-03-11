0de43c1b2f1b52dd3553829fbaa5f3c8f387afd0
Steve Wiggins <WigF1@users.noreply.github.com>
2025-03-11 16:38:49 +1000

    Add files via upload
    Added group creations, user creations

--- application-workloads/active-directory/active-directory-new-domain/DSC/CreateADPDC.ps1
+++ application-workloads/active-directory/active-directory-new-domain/DSC/CreateADPDC.ps1
@@ -1,37 +1,35 @@
 configuration CreateADPDC 
 { 
-   param 
-   ( 
+    param 
+    ( 
         [Parameter(Mandatory)]
         [String]$DomainName,
 
         [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$Admincreds,
 
-        [Int]$RetryCount=20,
-        [Int]$RetryIntervalSec=30
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
     ) 
     
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
-    $InterfaceAlias=$($Interface.Name)
+    $Interface = Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
 
     Node localhost
     {
-        LocalConfigurationManager 
-        {
+        LocalConfigurationManager {
             RebootNodeIfNeeded = $true
+            ActionAfterReboot = 'ContinueConfiguration'
         }
 
-	    WindowsFeature DNS 
-        { 
+        WindowsFeature DNS { 
             Ensure = "Present" 
-            Name = "DNS"		
+            Name   = "DNS"		
         }
 
-        Script GuestAgent
-        {
+        Script GuestAgent {
             SetScript  = {
                 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\WindowsAzureGuestAgent' -Name DependOnService -Type MultiString -Value DNS
                 Write-Verbose -Verbose "GuestAgent depends on DNS"
@@ -41,30 +39,28 @@ configuration CreateADPDC
             DependsOn  = "[WindowsFeature]DNS"
         }
 
-        Script EnableDNSDiags
-        {
-      	    SetScript = { 
-		        Set-DnsServerDiagnostics -All $true
+        Script EnableDNSDiags {
+            SetScript  = { 
+                Set-DnsServerDiagnostics -All $true
                 Write-Verbose -Verbose "Enabling DNS client diagnostics" 
             }
-            GetScript =  { @{} }
+            GetScript  = { @{} }
             TestScript = { $false }
-	    DependsOn = "[WindowsFeature]DNS"
+            DependsOn  = "[WindowsFeature]DNS"
         }
 
-	WindowsFeature DnsTools
-	{
-	    Ensure = "Present"
-            Name = "RSAT-DNS-Server"
+        WindowsFeature DnsTools {
+            Ensure    = "Present"
+            Name      = "RSAT-DNS-Server"
             DependsOn = "[WindowsFeature]DNS"
-	}
+        }
 
         xDnsServerAddress DnsServerAddress 
         { 
             Address        = '127.0.0.1' 
             InterfaceAlias = $InterfaceAlias
             AddressFamily  = 'IPv4'
-	    DependsOn = "[WindowsFeature]DNS"
+            DependsOn      = "[WindowsFeature]DNS"
         }
 
         xWaitforDisk Disk2
@@ -75,42 +71,125 @@ configuration CreateADPDC
         }
 
         xDisk ADDataDisk {
-            DiskNumber = 2
+            DiskNumber  = 2
             DriveLetter = "F"
-            DependsOn = "[xWaitForDisk]Disk2"
+            DependsOn   = "[xWaitForDisk]Disk2"
         }
 
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
-            Name = "AD-Domain-Services"
-	    DependsOn="[WindowsFeature]DNS" 
+        WindowsFeature ADDSInstall { 
+            Ensure    = "Present" 
+            Name      = "AD-Domain-Services"
+            DependsOn = "[WindowsFeature]DNS" 
         } 
 
-        WindowsFeature ADDSTools
-        {
-            Ensure = "Present"
-            Name = "RSAT-ADDS-Tools"
+        WindowsFeature ADDSTools {
+            Ensure    = "Present"
+            Name      = "RSAT-ADDS-Tools"
             DependsOn = "[WindowsFeature]ADDSInstall"
         }
 
-        WindowsFeature ADAdminCenter
-        {
-            Ensure = "Present"
-            Name = "RSAT-AD-AdminCenter"
+        WindowsFeature ADAdminCenter {
+            Ensure    = "Present"
+            Name      = "RSAT-AD-AdminCenter"
             DependsOn = "[WindowsFeature]ADDSInstall"
         }
          
         xADDomain FirstDS 
         {
-            DomainName = $DomainName
+            DomainName                    = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "F:\NTDS"
-            LogPath = "F:\NTDS"
-            SysvolPath = "F:\SYSVOL"
-	    DependsOn = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
+            DatabasePath                  = "F:\NTDS"
+            LogPath                       = "F:\NTDS"
+            SysvolPath                    = "F:\SYSVOL"
+            DependsOn                     = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
         } 
 
-   }
+        Configuration CheckReboot {
+            Import-DscResource -ModuleName xPendingReboot
+            Node localhost {
+                xPendingReboot RebootCheck {
+                    Name = "CheckReboot"
+                }
+            }
+        }
+
+        Script CreateOUs {
+            SetScript  = {
+                Import-Module ActiveDirectory
+                New-ADOrganizationalUnit 'ToSync' -path 'DC=adatum,DC=com' -ProtectedFromAccidentalDeletion $false
+                New-ADOrganizationalUnit 'AVDClients' -path 'DC=adatum,DC=com' -ProtectedFromAccidentalDeletion $false
+                Write-Verbose -Verbose "Created OUs"
+            }
+            GetScript  = { @{} }
+            TestScript = { $false }
+            DependsOn  = "[xADDomain]FirstDS"
+        }
+
+        Script CreateUsers {
+            SetScript  = {
+                $ouName = 'ToSync'
+                $ouPath = "OU=$ouName,DC=adatum,DC=com"
+                $adUserNamePrefix = 'aduser'
+                $adUPNSuffix = 'adatum.com'
+                $userCount = 1..9
+                foreach ($counter in $userCount) {
+                    New-AdUser -Name $adUserNamePrefix$counter -Path $ouPath -Enabled $True `
+                        -ChangePasswordAtLogon $false -userPrincipalName $adUserNamePrefix$counter@$adUPNSuffix `
+                        -AccountPassword (ConvertTo-SecureString 'Pa55w.rd1234!' -AsPlainText -Force) -passThru
+                } 
+
+                $adUserNamePrefix = 'avdadmin1'
+                $adUPNSuffix = 'adatum.com'
+                New-AdUser -Name $adUserNamePrefix -Path $ouPath -Enabled $True `
+                    -ChangePasswordAtLogon $false -userPrincipalName $adUserNamePrefix@$adUPNSuffix `
+                    -AccountPassword (ConvertTo-SecureString 'Pa55w.rd1234!' -AsPlainText -Force) -passThru
+
+            }
+            GetScript  = { @{} }
+            TestScript = { $false }
+            DependsOn  = '[Script]CreateOUs'
+        }
+
+        Script CreateGroups {
+            SetScript  = {
+                New-ADGroup -Name 'az140-avd-pooled' -GroupScope 'Global' -GroupCategory Security -Path $ouPath
+                New-ADGroup -Name 'az140-avd-remote-app' -GroupScope 'Global' -GroupCategory Security -Path $ouPath
+                New-ADGroup -Name 'az140-avd-personal' -GroupScope 'Global' -GroupCategory Security -Path $ouPath
+                New-ADGroup -Name 'az140-avd-users' -GroupScope 'Global' -GroupCategory Security -Path $ouPath
+                New-ADGroup -Name 'az140-avd-admins' -GroupScope 'Global' -GroupCategory Security -Path $ouPath
+            }
+            GetScript  = { @{} }
+            TestScript = { $false }
+            DependsOn  = '[Script]CreateUsers'
+        }
+
+        Script AddGroupMembera {
+            SetScript  = {
+                Add-ADGroupMember -Identity 'az140-avd-pooled' -Members 'aduser1', 'aduser2', 'aduser3', 'aduser4'
+                Add-ADGroupMember -Identity 'az140-avd-remote-app' -Members 'aduser1', 'aduser5', 'aduser6'
+                Add-ADGroupMember -Identity 'az140-avd-personal' -Members 'aduser7', 'aduser8', 'aduser9'
+                Add-ADGroupMember -Identity 'az140-avd-users' -Members 'aduser1', 'aduser2', 'aduser3', 'aduser4', 'aduser5', 'aduser6', 'aduser7', 'aduser8', 'aduser9'
+                Add-ADGroupMember -Identity 'az140-avd-admins' -Members 'avdadmin1'
+            }
+            GetScript  = { @{} }
+            TestScript = { $false }
+            DependsOn  = '[Script]CreateGroups'
+        }
+
+        Script InstallLibraries {
+            SetScript  = {
+                [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                Install-Module -Name PowerShellGet -Force -SkipPublisherCheck
+                Install-Module -Name Az -AllowClobber -SkipPublisherCheck -Force
+                Install-Module -Name AzureAD -Force -SkipPublisherCheck
+            }
+            GetScript  = { @{} }
+            TestScript = { $false }
+            DependsOn  = '[Script]AddGroupMembers'
+        }
+
+        
+
+    }
 } 
